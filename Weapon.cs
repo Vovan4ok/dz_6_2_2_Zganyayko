545f4b0513d67bb7a760e8e411c8f0635c6eddba
Volodymyr Zganyayko <bosik_ck@ukr.net>
2023-03-22 23:14:17 +0300

    add new file

---
+++ Weapon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public class Weapon
+    {
+        private string name;
+        private int damage;
+        public Weapon(string name, int damage)
+        {
+            this.name = name;
+            this.damage = damage;
+        }
+        public string getName() => this.name;
+        public void setName(string name) => this.name = name;
+        public int getDamage() => this.damage;
+        public void setDamage(int damage) => this.damage = damage;
+    }
+}
